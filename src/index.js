ea304338659f2f7ebe0df758ffbbe38980c86d5f
drogba <drogba02@naver.com>
2020-01-04 18:04:45 +0900

    design edit and footer add

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {MuiThemeProvider , createMuiTheme} from '@material-ui/core/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: '"Noto Sans KR" , serif',
+    },
+});
+
+ReactDOM.render(<MuiThemeProvider theme={theme}><App /></MuiThemeProvider> ,document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
 
 
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,15 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
-class Footer extends Component{
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    minWidth: 1080
+  }
+})
+
+class Footer extends Component{  
+  state = {
+    value: 0
+  }
   render(){
+    const {classes} = this.props;
+    const value = this.state;
+
     return(
-      <footer>
-        <div>
-          Copyright © Your Website 2019
-        </div>
-      </footer>
+      <BottomNavigation
+            value={value}
+            onChange={(event, newValue) => {
+              
+            }}
+            showLabels
+            className={classes.root}
+          >
+            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+          </BottomNavigation>
     );
   }
 }
 
-export default Footer;
+export default withStyles(styles)(Footer);
